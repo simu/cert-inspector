efb72e2b9d3ab37897fd84e8b6ab7ee6b5317859
Simon Gerber <simon.gerber@vshn.ch>
2025-01-10 13:08:20 +0100

    Refactor and introduce subcommands
    Subcommand `split` takes a file which contains one or more PEM-encoded
    certificates and writes each certificate into a numbered file.
    
    Subcommand `info` takes a file which contains one or more PEM-encoded
    certificates and prints basic info (subject, issuer, validity and DNS
    alt names) for each certificate.

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,43 @@
 use std::{
     fs::OpenOptions,
-    io::{BufWriter, Write},
+    io::{BufReader, BufWriter, Write},
+    net::{Ipv4Addr, Ipv6Addr},
+    path::{Path, PathBuf},
 };
 
-use clap::Parser;
+use clap::{Parser, Subcommand};
+use oid_registry::OID_X509_EXT_SUBJECT_ALT_NAME;
+use rustls_pki_types::CertificateDer;
+use x509_parser::prelude::*;
 
 #[derive(Parser)]
 struct Cli {
-    /// The ca-bundle to split
-    path: std::path::PathBuf,
-    /// The output file prefix
-    output_prefix: Option<String>,
+    #[clap(subcommand)]
+    command: Command,
 }
-fn main() {
-    let args = Cli::parse();
-    println!(
-        "Called with {:?} and output pattern {:?}",
-        args.path, args.output_prefix,
-    );
 
-    let cabundle = std::fs::read(&args.path).expect("Reading file");
+#[derive(Subcommand)]
+enum Command {
+    /// Split ca-bundle into separate files
+    Split {
+        /// The ca-bundle to split
+        path: PathBuf,
+        /// The output file prefix
+        output_prefix: Option<String>,
+    },
+    /// Print information about each certificate in the bundle
+    Info {
+        /// The ca-bundle to split
+        path: PathBuf,
+    },
+}
 
-    for (i, cert) in pem::parse_many(&cabundle[0..]).unwrap().iter().enumerate() {
-        let p = if let Some(prefix) = &args.output_prefix {
+fn split_bundle(bundle: &[u8], path: &Path, output_prefix: &Option<String>) {
+    for (i, cert) in ::pem::parse_many(bundle).unwrap().iter().enumerate() {
+        let p = if let Some(prefix) = output_prefix {
             format!("{prefix}-{i}.crt")
         } else {
-            let base = args
-                .path
+            let base = path
                 .file_stem()
                 .expect("stem")
                 .to_str()
@@ -36,10 +47,81 @@ fn main() {
         let f = OpenOptions::new()
             .write(true)
             .create(true)
+            .truncate(true)
             .open(p)
             .expect("opening output file");
         let mut outbuf = BufWriter::new(f);
         write!(&mut outbuf, "{cert}").expect("Writing cert");
         outbuf.flush().expect("Flushing file");
+    }
+}
+
+fn extract_dns_names(cert: &X509Certificate) -> Vec<String> {
+    if let Ok(Some(dns_ext)) = cert.get_extension_unique(&OID_X509_EXT_SUBJECT_ALT_NAME) {
+        match dns_ext.parsed_extension() {
+            ParsedExtension::SubjectAlternativeName(san) => {
+                let mut res = vec![];
+                for name in &san.general_names {
+                    match name {
+                        GeneralName::DNSName(dns) => res.push(dns.to_string()),
+                        GeneralName::IPAddress(b) => {
+                            let ip = match b.len() {
+                                4 => {
+                                    let b = <[u8; 4]>::try_from(*b).unwrap();
+                                    let ip = Ipv4Addr::from(b);
+                                    format!("{}", ip)
+                                }
+                                16 => {
+                                    let b = <[u8; 16]>::try_from(*b).unwrap();
+                                    let ip = Ipv6Addr::from(b);
+                                    format!("{}", ip)
+                                }
+                                l => format!("invalid (len={})", l),
+                            };
+                            res.push(ip.to_string())
+                        }
+                        _ => res.push(format!("{name:?}")),
+                    }
+                }
+                res
+            }
+            _ => vec![],
+        }
+    } else {
+        vec![]
+    }
+}
+
+fn cert_info(cert: &CertificateDer) {
+    let (_, c) = X509Certificate::from_der(cert.as_ref()).expect("Parsing certificate");
+    println!("Subject:     {}", c.subject);
+    println!("Issuer:      {}", c.issuer);
+    println!("Not Before:  {}", c.validity.not_before);
+    println!("Not After:   {}", c.validity.not_after);
+    println!("DNS names:   {:?}", extract_dns_names(&c));
+}
+
+fn main() {
+    let args = Cli::parse();
+
+    match &args.command {
+        Command::Split {
+            path,
+            output_prefix,
+        } => {
+            let cabundle = std::fs::read(path).expect("Reading file");
+
+            split_bundle(&cabundle, path, output_prefix);
+        }
+        Command::Info { path } => {
+            let bundlefile = std::fs::File::open(path).expect("opening file");
+            let mut buf = BufReader::new(bundlefile);
+            for (i, cert) in rustls_pemfile::certs(&mut buf).enumerate() {
+                if let Ok(cert) = cert {
+                    println!("Certificate {i}:");
+                    cert_info(&cert);
+                }
+            }
+        }
     }
 }
